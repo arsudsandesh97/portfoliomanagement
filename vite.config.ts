c8a7f0e3222f6fe24933216f281571fca1bfa279
Sandesh Arsud <arsudsandesh97@gmail.com>
2026-05-20 13:07:39 +0530

    Change Base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://arsudsandesh97.github.io/portfoliomanagement/",
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
